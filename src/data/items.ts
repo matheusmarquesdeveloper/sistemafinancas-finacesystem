10add0171a7b6f51c68fc26f8e7fb5fca554f324
Matheus Marques <matheusmdev97@outlook.com>
2023-05-30 19:24:35 -0300

    [style]financesystem

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 6), category: 'food', title: 'McDonalds', value: 30 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Mercado', value: 280 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel', value: 2300 },
+    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário', value: 4500 },
+];
